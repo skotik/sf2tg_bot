3ac172b26ec710a8f088ada52d0abc175b20dc68
skotik <skotik@users.noreply.github.com>
2023-11-29 16:18:40 +1200

    Create my.py
    Файл настроек

---
+++ my.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+#Группы в которые отслеживаем изменения
+group_id=[-205879084,2000000001,2000000002] 
+# id группы или пользователя, куда посылать изменения
+tg_id='123456'
+#Токен телеграмм бота
+bot_token='111:ZZZ-BBB'
+#
+headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36"}
+# cookie с названием remixdsid от пользователя из под которого отслеживаем изменения
+cookie = {'Cookie': 'remixdsid=vk1.a.набор символов'}
